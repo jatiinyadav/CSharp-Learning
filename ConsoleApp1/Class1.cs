7d804b2ef71ab481d114232f8239f55d9b43a085
jatiinyadav <jatin27yadav@gmail.com>
2023-01-05 19:53:50 +0530

    Add project files.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+
+        int TutorialId;
+        string TutorialName;
+
+        public void Settutorial(int id, string name)
+        {
+
+        }
+
+        public String getTutorialName()
+        {
+            return TutorialName;
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Program
+{
+
+    class Prog2 {
+        public int num = 10;
+        public string str = "Jatin Yadav";
+        public double db = 23.33;
+        public bool bl = false;
+        public char ch = 'a';
+
+    }
+    class Prog
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello You There");
+
+            Prog2 prog2 = new();
+
+            int number = 11;
+
+            // If loop
+            if (number > 12)
+            {
+                Console.WriteLine("Greater");
+            } else
+            {
+                Console.WriteLine("Smaller");
+            }
+
+            // Switch
+            switch (number)
+            {
+                case 10: Console.WriteLine("10 value is there");
+                    break;
+                case 11: Console.WriteLine("11 value is there");
+                    break;
+            }
+
+            // 
+            int[] arr = new int[4];
+            arr[0] = 3;
+            arr[1] = 4;
+            arr[2] = 6;
+
+            Console.WriteLine(string.Join(", ", arr));
+
+        }
+    }
+}
